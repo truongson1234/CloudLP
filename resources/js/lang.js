2d4e58ded5e2029198a96e761480fea015962c4d
sonng147202 <sonnguyen20140720@gmail.com>
2022-12-15 12:29:52 +0700

    update

---
+++ resources/js/lang.js
@@ -0,0 +1,23 @@
+import langEng from '../lang/eng.json' assert {type: 'json'};
+import langVie from '../lang/vie.json' assert {type: 'json'};
+
+// console.log(langVi.translation);
+i18next.init({
+    lng: 'eng',
+    resources: {
+        eng: {
+            translation: langEng.translation
+        },
+        vie: {
+            translation: langVie.translation
+        },
+    }
+}, function(err, t) {
+    i18nextJquery.init(i18next, $);
+    $("#main").localize();
+
+    $('.header .header__lang .header__lang-link').click(function() {
+        i18next.changeLanguage(this.innerHTML);
+        $("#main").localize();
+    });
+});
