8c717ccf61c6919a2f04b989103a3175b438f321
sonng147202 <sonnguyen20140720@gmail.com>
2022-12-22 00:49:49 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ doc.ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 1860,
         prevArrow: '<button type="button" class="slick-prev carousel-btn carousel-btn-prev" style="width: 60px; height: 60px;"><i class="fa-regular fa-arrow-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next carousel-btn carousel-btn-next" style="width: 60px; height: 60px;"><i class="fa-regular fa-arrow-right"></i></button>',
         responsive: []
